d643fe209df224aabfb902a636059104029c4bee
Mohammed-Ali Garouj <mgarouj@e1r7p14.1337.ma>
2025-11-12 18:10:45 +0100

    learn exception

--- test.cpp
+++ test.cpp
@@ -50,46 +50,92 @@ using namespace std;
 
 
 
+// int main(void)
+// {
+//     int x, y;
+//     cout << "enter x : " << endl;
+//     cin >> x;
+//     cout << "enter y : " << endl;
+//     cin >> y;
+//     cout << endl << endl << endl;
+//     // cout << x / y << endl;
+//     try{
+//         if (!y)
+//         {
+//             // cout << "y is not valide " << endl;
+//             throw "division by zero !";
+//         }
+//         if (x > 100)
+//             throw "x older tha 100 !";
+//         if (y > 200)
+//             throw y / x;
+//         if (y < 0)
+//             throw exception();
+//         cout << x / y << endl;
+
+//     }
+//     catch(const char * e)
+//     {
+//         cout << e << endl;
+//         cout << "try to enter values other than 0!" << endl;
+//     }
+//     catch (int num)
+//     {
+
+//         cout <<" catch for num " << num << endl;
+//     }
+
+//     catch (...)
+//     {
+//         cout << "error , try again (random issue )" << endl;
+//     }
+
+//     cout << "hello world " << endl;
+//     return 0;
+// }
+
+
+int compareage(int sonAge, int momAge)
+{
+    if (sonAge >= momAge)
+        throw 1;
+    else if (sonAge <= 0)
+        throw 2;
+    else if (momAge <= 0)
+            throw 3;
+    else if (momAge - sonAge < 12)
+        throw 4;
+    return momAge - sonAge;
+}
+
+
 int main(void)
 {
-    int x, y;
-    cout << "enter x : " << endl;
-    cin >> x;
-    cout << "enter y : " << endl;
-    cin >> y;
-    cout << endl << endl << endl;
-    // cout << x / y << endl;
-    try{
-        if (!y)
+    try
+    {
+        compareage(26, 24);
+    }
+    catch (int e)
+    {
+        switch (e)
         {
-            // cout << "y is not valide " << endl;
-            throw "division by zero !";
+        case 1:
+            cout << "Error: son's age can't be less than his mom! \n" << endl;
+            break;
+        case 2:
+            cout << "Error: Son's age can't be less than or equal zero \n" << endl;
+            break;
+        case 3:
+            cout <<  "Error: Mom's age can't be less than or equal zero \n" << endl;
+            break;
+        case 4:
+            cout << "Error: Mom's age should be 12 years bigger than son age \n" << endl;
+            break;
         }
-        if (x > 100)
-            throw "x older tha 100 !";
-        if (y > 200)
-            throw y / x;
-        if (y < 0)
-            throw exception();
-        cout << x / y << endl;
-
-    }
-    catch(const char * e)
-    {
-        cout << e << endl;
-        cout << "try to enter values other than 0!" << endl;
-    }
-    catch (int num)
-    {
-
-        cout <<" catch for num " << num << endl;
     }
 
-    catch (...)
-    {
-        cout << "error , try again (random issue )" << endl;
-    }
-
-    cout << "hello world " << endl;
+    cout << "the program is still working properly " << endl;
+    
     return 0;
+    
 }
