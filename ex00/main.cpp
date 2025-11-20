59c93366d072228e8424bfc94abee4dfaa6572ab
mohammed ali <mohammedgarouj7@gmail.com>
2025-11-20 23:11:22 +0100

    add some test in main in ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -7,8 +7,19 @@ int main()
     {
         Bureaucrat ob1("ali", 50);
         std::cout << ob1 << std::endl;
-        Bureaucrat ob2("simohammed", 0);
+
+        Bureaucrat ob2("simohammed", 1);
         std::cout << ob2 << std::endl;
+        
+        ob2.decrementGrade();
+        std::cout << ob2 << std::endl;
+        
+        ob2.incrementGrade();
+        std::cout << ob2 << std::endl;
+    
+        ob2.incrementGrade();
+        std::cout << ob2 << std::endl;
+
     }
     catch(const std::exception& e)
     {
