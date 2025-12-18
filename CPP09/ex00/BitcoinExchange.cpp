fd4ea59d3781bf27ab3d2d63c377c9537265fd33
mohammed ali <mohammedgarouj7@gmail.com>
2025-12-18 13:01:45 +0100

    bitcoinexchange.cpp

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -20,5 +20,14 @@ BitcoinExchange::~BitcoinExchange(){}
 
 void BitcoinExchange::loadDataBase(const std::string& fileName)
 {
-    std::istream nameData(fileName);
+    std::ifstream file(fileName);
+    if (!file.is_open())
+    {
+        std::cout << "Error: could not open file. " << std::endl;
+        exit(1);
+    }
+
+    std::string line;
+
+    std::getline(file, line);
 }
